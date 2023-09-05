4c9a5b63eb824abdb2d081e108b29fd0dbe524bb
aaaravv <aaravmishra619@gmail.com>
2023-09-05 09:44:28 +0530

    feat: add new setup page with initial profile creation

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  // find this prfile if it is a member of a server
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile?.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    // redirect to the server
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <div className="">Create a new Server</div>;
+};
+
+export default SetupPage;
---
+++ lib/initial-profile.ts
@@ -0,0 +1,31 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export const initialProfile = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    redirectToSignIn();
+    return;
+  }
+
+  const profile = await db.profile.findUnique({
+    where: { userId: user.id },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      email: user.emailAddresses[0].emailAddress,
+      imageUrl: user.imageUrl,
+    },
+  });
+
+  return newProfile;
+};
