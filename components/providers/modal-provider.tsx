774d4cd15987a599d5c7e808bbef4953d3ff469d
aaaravv <aaravmishra619@gmail.com>
2023-09-12 09:05:46 +0530

    feat: add new route for updating server details

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 
 import CreateServerModal from "@/components/modals/create-server-modal";
+import EditServerModal from "@/components/modals/edit-server-modal";
+import InviteModal from "@/components/modals/invite-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -18,6 +20,8 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
+      <EditServerModal />
     </>
   );
 };
--- hooks/use-modal-store.tsx
+++ hooks/use-modal-store.tsx
@@ -1,17 +1,24 @@
+import { Server } from "@prisma/client";
 import { create } from "zustand";
 
-export type ModalType = "createServer";
+export type ModalType = "createServer" | "invite" | "editServer";
 
 interface ModalStore {
   type: ModalType | null;
   isOpen: boolean;
-  onOpen: (modal: ModalType) => void;
+  data: ModalData;
+  onOpen: (modal: ModalType, data?: ModalData) => void;
   onClose: () => void;
+}
+
+interface ModalData {
+  server?: Server;
 }
 
 export const useModal = create<ModalStore>((set) => ({
   type: null,
   isOpen: false,
-  onOpen: (type: ModalType) => set({ type, isOpen: true }),
+  data: {},
+  onOpen: (type: ModalType, data = {}) => set({ type, isOpen: true, data }),
   onClose: () => set({ type: null, isOpen: false }),
 }));
