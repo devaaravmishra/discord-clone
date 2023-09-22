6d8b1fb2d31401a68a72963026168c82fc3ec46e
aaaravv <aaravmishra619@gmail.com>
2023-09-22 09:33:25 +0530

    chore: webpack config tweaks for external dependencies
    Updated the webpack configuration to include `utf-8-validate` and `bufferutil` as external dependencies.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["uploadthing.com", "utfs.io"],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+	webpack: (config) => {
+		config.externals.push({
+			"utf-8-validate": "commonjs utf-8-validate",
+			bufferutil: "commonjs bufferutil",
+		});
+
+		return config;
+	},
+	images: {
+		domains: ["uploadthing.com", "utfs.io"],
+	},
 };
 
 module.exports = nextConfig;
