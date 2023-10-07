e164ac332ed49cea3f652d2f97b7292142aa60b2
aaaravv <aaravmishra619@gmail.com>
2023-10-07 08:55:26 +0530

    chore: fix typo in layout styles

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,18 +2,18 @@ import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 import React from "react";
 
 interface MainLayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const MainLayout = async ({ children }: MainLayoutProps) => {
-  return (
-    <div className="h-full">
-      <div className="hidden md:flex h-full w[72px] z-30 flex-col fixed inset-y-0">
-        <NavigationSidebar />
-      </div>
-      <main className="md:pl-[72px] h-full">{children}</main>
-    </div>
-  );
+	return (
+		<div className="h-full">
+			<div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+				<NavigationSidebar />
+			</div>
+			<main className="md:pl-[72px] h-full">{children}</main>
+		</div>
+	);
 };
 
 export default MainLayout;
