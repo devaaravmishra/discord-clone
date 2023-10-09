c040c79f00acbc782279ce9600bdf0b09eb6af78
aaaravv <aaravmishra619@gmail.com>
2023-10-09 08:53:30 +0530

    refactor: update patch and delete functions in member route
    - Handle unauthorized and bad request cases by returning appropriate responses.

--- app/api/members/[memberId]/route.ts
+++ app/api/members/[memberId]/route.ts
@@ -3,123 +3,131 @@ import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
-  req: Request,
-  params: {
-    memberId: string;
-  },
+	req: Request,
+	{
+		params,
+	}: {
+		params: {
+			memberId: string;
+		};
+	},
 ) {
-  try {
-    const profile = await currentProfile();
-    const { searchParams } = new URL(req.url);
-    const { role } = await req.json();
+	try {
+		const profile = await currentProfile();
+		const { searchParams } = new URL(req.url);
+		const { role } = await req.json();
 
-    const serverId = searchParams.get("serverId");
+		const serverId = searchParams.get("serverId");
 
-    if (!profile) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+		if (!profile) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
 
-    if (!serverId || !params.memberId) {
-      return new NextResponse("Bad request", { status: 400 });
-    }
+		if (!serverId || !params.memberId || !role) {
+			return new NextResponse("Bad request", { status: 400 });
+		}
 
-    const server = await db.server.update({
-      where: {
-        id: serverId,
-        profileId: profile.id,
-      },
-      data: {
-        members: {
-          update: {
-            where: {
-              id: params.memberId,
-              profileId: {
-                not: profile.id,
-              },
-            },
-            data: {
-              role,
-            },
-          },
-        },
-      },
-      include: {
-        members: {
-          include: {
-            profile: true,
-          },
-          orderBy: {
-            role: "asc",
-          },
-        },
-      },
-    });
+		const server = await db.server.update({
+			where: {
+				id: serverId,
+				profileId: profile.id,
+			},
+			data: {
+				members: {
+					update: {
+						where: {
+							id: params.memberId,
+							profileId: {
+								not: profile.id,
+							},
+						},
+						data: {
+							role,
+						},
+					},
+				},
+			},
+			include: {
+				members: {
+					include: {
+						profile: true,
+					},
+					orderBy: {
+						role: "asc",
+					},
+				},
+			},
+		});
 
-    if (!server) {
-      return new NextResponse("Not found", { status: 404 });
-    }
+		if (!server) {
+			return new NextResponse("Not found", { status: 404 });
+		}
 
-    return NextResponse.json(server);
-  } catch (error) {
-    console.error("[MEMBERS_ID_PATCH]", error);
-    return new NextResponse("Internal server error", { status: 500 });
-  }
+		return NextResponse.json(server);
+	} catch (error) {
+		console.error("[MEMBERS_ID_PATCH]", error);
+		return new NextResponse("Internal server error", { status: 500 });
+	}
 }
 
 export async function DELETE(
-  req: Request,
-  params: {
-    memberId: string;
-  },
+	req: Request,
+	{
+		params,
+	}: {
+		params: {
+			memberId: string;
+		};
+	},
 ) {
-  try {
-    const profile = await currentProfile();
-    const { searchParams } = new URL(req.url);
+	try {
+		const profile = await currentProfile();
+		const { searchParams } = new URL(req.url);
 
-    const serverId = searchParams.get("serverId");
+		const serverId = searchParams.get("serverId");
 
-    if (!profile) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+		if (!profile) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
 
-    if (!serverId || !params.memberId) {
-      return new NextResponse("Bad request", { status: 400 });
-    }
+		if (!serverId || !params.memberId) {
+			return new NextResponse("Bad request", { status: 400 });
+		}
 
-    const server = await db.server.update({
-      where: {
-        id: serverId,
-        profileId: profile.id,
-      },
-      data: {
-        members: {
-          deleteMany: {
-            id: params.memberId,
-            profileId: {
-              not: profile.id,
-            },
-          },
-        },
-      },
-      include: {
-        members: {
-          include: {
-            profile: true,
-          },
-          orderBy: {
-            role: "asc",
-          },
-        },
-      },
-    });
+		const server = await db.server.update({
+			where: {
+				id: serverId,
+				profileId: profile.id,
+			},
+			data: {
+				members: {
+					deleteMany: {
+						id: params.memberId,
+						profileId: {
+							not: profile.id,
+						},
+					},
+				},
+			},
+			include: {
+				members: {
+					include: {
+						profile: true,
+					},
+					orderBy: {
+						role: "asc",
+					},
+				},
+			},
+		});
 
-    if (!server) {
-      return new NextResponse("Not found", { status: 404 });
-    }
+		if (!server) {
+			return new NextResponse("Not found", { status: 404 });
+		}
 
-    return NextResponse.json(server);
-  } catch (error) {
-    console.error("[MEMBERS_ID_DELETE]", error);
-    return new NextResponse("Internal server error", { status: 500 });
-  }
+		return NextResponse.json(server);
+	} catch (error) {
+		console.error("[MEMBERS_ID_DELETE]", error);
+		return new NextResponse("Internal server error", { status: 500 });
+	}
 }
