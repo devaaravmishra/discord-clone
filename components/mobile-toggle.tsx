e8070cfa5b3a956954b5ea5de3bbf2ab721ffc70
aaaravv <aaravmishra619@gmail.com>
2023-11-23 09:46:15 +0530

    styles: tweak spacing issue for hamburger

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -6,21 +6,21 @@ import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const MobileToggle = ({ serverId }: { serverId: string }) => {
-  return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="ghost" size="icon" className="md:hidden">
-          <Menu className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left" className="p-0 flex gap-0">
-        <div className="w-[72px]">
-          <NavigationSidebar />
-        </div>
-        <ServerSidebar serverId={serverId} />
-      </SheetContent>
-    </Sheet>
-  );
+	return (
+		<Sheet>
+			<SheetTrigger asChild>
+				<Button variant="ghost" size="icon" className="md:hidden">
+					<Menu className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+				</Button>
+			</SheetTrigger>
+			<SheetContent side="left" className="p-0 flex gap-0">
+				<div className="w-[72px]">
+					<NavigationSidebar />
+				</div>
+				<ServerSidebar serverId={serverId} />
+			</SheetContent>
+		</Sheet>
+	);
 };
 
 export default MobileToggle;
